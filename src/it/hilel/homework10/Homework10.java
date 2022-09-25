9688d6fb07f697d70a32d31ec5ca6d10d1a75d94
Kostantin Kava <110024628+kostantink89@users.noreply.github.com>
2022-09-25 22:20:43 +0300

    HW10: "Solution code"

---
+++ src/it/hilel/homework10/Homework10.java
@@ -0,0 +1,13 @@
+package it.hilel.homework10;
+
+public class Homework10 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if (i / 10 != 4 && i / 10 != 9 && i % 10 != 4 && i % 10 != 9 && (i / 10 % 10 != 4)) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
+
